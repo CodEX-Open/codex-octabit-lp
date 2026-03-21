b32b15ec54dad6929a91794b773c25dd0a1dbe4f
Otavio Demarchi <otaviolunardimoreno@gmail.com>
2026-03-21 16:36:10 -0300

    feat: Tela escurece quando menu hamburger abre + Fecha ao clicar fora
    O resto do site escurece ao abrir o menu hamburger, caso o usuario aperte fora da tela do menu, o menu fecha.
    
    Logo do octabit continua acesa propositalmente, possivel mudar depois caso não gostem

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,15 @@
 const hamburger = document.getElementById('hamburger');
 const nav = document.querySelector('.header__nav');
+const overlay = document.getElementById('overlay');
 
 hamburger.addEventListener('click', () => {
     nav.classList.toggle('active');
     hamburger.classList.toggle('active');
+    overlay.classList.toggle('active');
+});
+
+overlay.addEventListener('click', () => {
+    nav.classList.remove('active');
+    hamburger.classList.remove('active');
+    overlay.classList.remove('active');
 });
